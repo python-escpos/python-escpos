5bf26367538f9ffb572659b3c49582aa29dbff2a
Patrick Kanzler <dev@pkanzler.de>
2017-05-22 00:44:22 +0200

    rewrite to Dummy()

--- test/test_function_panel_button.py
+++ test/test_function_panel_button.py
@@ -12,43 +12,18 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from nose.tools import with_setup
-
 import escpos.printer as printer
-import os
-
-devfile = 'testfile'
 
 
-def setup_testfile():
-    """create a testfile as devfile"""
-    fhandle = open(devfile, 'a')
-    try:
-        os.utime(devfile, None)
-    finally:
-        fhandle.close()
-
-
-def teardown_testfile():
-    """destroy testfile again"""
-    os.remove(devfile)
-
-
-@with_setup(setup_testfile, teardown_testfile)
 def test_function_panel_button_on():
     """test the panel button function (enabling) by comparing output"""
-    instance = printer.File(devfile=devfile)
+    instance = printer.Dummy()
     instance.panel_buttons()
-    instance.flush()
-    with open(devfile, "rb") as f:
-        assert(f.read() == b'\x1B\x63\x35\x00')
+    assert(instance.output == b'\x1B\x63\x35\x00')
 
 
-@with_setup(setup_testfile, teardown_testfile)
 def test_function_panel_button_off():
     """test the panel button function (disabling) by comparing output"""
-    instance = printer.File(devfile=devfile)
+    instance = printer.Dummy()
     instance.panel_buttons(False)
-    instance.flush()
-    with open(devfile, "rb") as f:
-        assert(f.read() == b'\x1B\x63\x35\x01')
+    assert(instance.output == b'\x1B\x63\x35\x01')
