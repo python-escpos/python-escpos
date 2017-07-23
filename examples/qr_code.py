b5349960e51b007e3cd0c6530321b8462b35cf33
Romain Porte <microjoe@microjoe.org>
2017-07-23 10:47:41 +0200

    Let the user print stuff using qr example

--- examples/qr_code.py
+++ examples/qr_code.py
@@ -1,16 +1,19 @@
+import sys
+
 from escpos.printer import Usb
 
 
+def usage():
+    print("usage: qr_code.py <content>")
+
+
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        usage()
+        sys.exit(1)
+
+    content = sys.argv[1]
 
     # Adapt to your needs
     p = Usb(0x0416, 0x5011, profile="POS-5890")
-
-    # Some software barcodes
-    p.set(align='left')
-
-    p.text("hello\n")
-
-    p.qr("https://github.com/python-escpos/python-escpos")
-
-    p.text("world\n")
+    p.qr(content)
