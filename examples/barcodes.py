c390b319e4c9d084d65d107fa7ca3617e6d0dad7
Romain Porte <microjoe@microjoe.org>
2017-07-27 21:53:01 +0200

    Added test script for hard and soft barcodes

---
+++ examples/barcodes.py
@@ -0,0 +1,11 @@
+from escpos.printer import Usb
+
+
+# Adapt to your needs
+p = Usb(0x0416, 0x5011, profile="POS-5890")
+
+# Print software and then hardware barcode with the same content
+p.soft_barcode('code39', '123456')
+p.text('\n')
+p.text('\n')
+p.barcode('123456', 'CODE39')
