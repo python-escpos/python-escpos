6bca277f52e454b7b74333448eac8ddfbdf1121f
Christoph Heuel <mail@christoph-heuel.net>
2017-11-25 02:31:50 +0100

    Add Test Function for dummy.clear

---
+++ test/test_function_dummy_clear.py
@@ -0,0 +1,8 @@
+from nose.tools import assert_raises
+from escpos.printer import Dummy
+
+def test_printer_dummy_clear():
+    printer = Dummy()
+    printer.text("Hello")
+    printer.clear()
+    assert(printer.output == b'')
