cb30d7a8819abc258b2340dcf141e04798e3a3d0
Romain Porte <microjoe@microjoe.org>
2019-10-07 07:43:23 +0200

    drop Python 2.7 support
    Python 2.7 EOL is arriving on 2020-01-01: https://pythonclock.org/
    
    This will allow us to use Python 3 only libraries, like python-barcode,
    which can maintain a reduced, simpler codebase, due to only one version
    to support.
    
    Closes #371.
    
    Signed-off-by: Romain Porte <microjoe@microjoe.org>

--- src/escpos/exceptions.py
+++ src/escpos/exceptions.py
@@ -25,11 +25,6 @@ Result/Exit codes:
 :license: MIT
 """
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 
 class Error(Exception):
     """ Base class for ESC/POS errors """
--- test/test_function_cashdraw.py
+++ test/test_function_cashdraw.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
 
 import escpos.printer as printer
 from escpos.exceptions import CashDrawerError
