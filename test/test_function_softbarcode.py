8ca682e3acf19f0b599235b22d9fb711c368e047
Romain Porte <microjoe@microjoe.org>
2020-05-10 14:06:00 +0200

    soft_barcode: add new center=True option

--- test/test_function_softbarcode.py
+++ test/test_function_softbarcode.py
@@ -12,3 +12,6 @@ def instance():
 def test_soft_barcode_ean8(instance):
     instance.soft_barcode("ean8", "1234")
 
+
+def test_soft_barcode_ean8_nocenter(instance):
+    instance.soft_barcode("ean8", "1234", center=False)
