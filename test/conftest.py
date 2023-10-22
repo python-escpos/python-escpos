32cc334dd16e79326373bb08904c0dd9ed7f2bb4
belono <tiotil.lindeman@gmail.com>
2023-10-22 12:44:06 +0200

    Add fixtures

--- test/conftest.py
+++ test/conftest.py
@@ -1,8 +1,48 @@
 import pytest
-
-from escpos.printer import Dummy
+from escpos.exceptions import DeviceNotFoundError
+from escpos.printer import LP, CupsPrinter, Dummy, File, Network, Serial, Usb, Win32Raw
 
 
 @pytest.fixture
 def driver():
     return Dummy()
+
+
+@pytest.fixture
+def usbprinter():
+    return Usb()
+
+
+@pytest.fixture
+def serialprinter():
+    return Serial()
+
+
+@pytest.fixture
+def networkprinter():
+    return Network()
+
+
+@pytest.fixture
+def fileprinter():
+    return File()
+
+
+@pytest.fixture
+def lpprinter():
+    return LP()
+
+
+@pytest.fixture
+def win32rawprinter():
+    return Win32Raw()
+
+
+@pytest.fixture
+def cupsprinter():
+    return CupsPrinter()
+
+
+@pytest.fixture
+def devicenotfounderror():
+    return DeviceNotFoundError
