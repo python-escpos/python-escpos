125a0786304addf04ff75d43ed8cd1cfa98a830f
Patrick Kanzler <dev@pkanzler.de>
2023-12-16 22:21:41 +0100

    add none annotations

--- test/test_functions/test_function_cut.py
+++ test/test_functions/test_function_cut.py
@@ -2,7 +2,7 @@ import escpos.printer as printer
 from escpos.constants import GS
 
 
-def test_cut_without_feed():
+def test_cut_without_feed() -> None:
     """Test cut without feeding paper"""
     instance = printer.Dummy()
     instance.cut(feed=False)
