c05201032ced0a5ee7adc8f63d1e744bcc4cfd60
hasansezertasan <hasansezertasan@gmail.com>
2025-09-08 01:35:05 +0300

    Update linting configuration to include 'I' category and clean up imports in Flask app and CLI module

--- src/escpos/cli.py
+++ src/escpos/cli.py
@@ -20,9 +20,8 @@ except ImportError:
     pass  # noqa
 import sys
 
-from . import config, escpos
+from . import config, escpos, version
 from . import printer as escpos_printer_module
-from . import version
 
 
 # Must be defined before it's used in DEMO_FUNCTIONS
